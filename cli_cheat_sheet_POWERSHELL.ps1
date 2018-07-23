bb490104773029404a01dc2871f17d5b64997e29
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-07-23 11:39:24 -0500

    Moved from .txt file

---
+++ cli_cheat_sheet_POWERSHELL.ps1
@@ -0,0 +1,26 @@
+exit
+####################
+## PPPPPP	 SSSSS
+## P	 P	S
+## PPPPPP	 SSSSS
+## P 	 		  S
+## P 		 SSSSS
+####################
+# WINDOWS SERVER POWERSHELL
+# .ps1
+
+
+# Single line comment
+#my comment
+
+# Multiline comment
+<#
+my comment
+#>
+
+# Prefix to call an environmental variable
+$Env:
+#ex: $Env:SystemRoot
+
+# How to find what version of PowerShell you are running:
+$psversiontable
