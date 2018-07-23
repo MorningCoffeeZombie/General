f5bc0d034342fcdb5baeb2d90976b283cb7008e4
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-07-23 11:38:25 -0500

    Moved from .txt and fixed some formatting

---
+++ cli_cheat_sheet_BASH.sh
@@ -0,0 +1,62 @@
+exit
+#########################################
+## BBBBB	    A		 SSSSS		H	H
+## B	B	   A A		S			H	H
+## BBBBB	  A   A		 SSSSS		HHHHH
+## B	B	 AAAAAAA		  S		H	H
+## BBBBB	A		A	 SSSSS		H	H
+#########################################
+# LINUX SHELL
+# .run .sh
+
+
+# Prefix to allow script execution
+#!/bin/bash
+
+# Pause currently running command (2 variants)
+$HOST.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | OUT-NULL
+$HOST.UI.RawUI.Flushinputbuffer()
+
+cmd /c pause | out-null
+
+# Create a variable named $TODAY with the date in ISO format
+TODAY=`date '+%Y%m%d-%H%M'`
+
+# Say the name of this current script. Multiple variants:
+echo
+echo "# arguments called with ---->  ${@}     "
+echo "# \$1 ---------------------->  $1       "
+echo "# \$2 ---------------------->  $2       "
+echo "# path to me --------------->  ${0}     "
+echo "# parent path -------------->  ${0%/*}  "
+echo "# my name ------------------>  ${0##*/} "
+echo
+
+# Pass command output to null
+#The > operator redirects the output usually to a file but it can be to a device. You can also use >> to append.
+#If you don't specify a number then the standard output stream is assumed but you can also redirect errors
+> file redirects stdout to file
+1> file redirects stdout to file
+2> file redirects stderr to file
+&> file redirects stdout and stderr to file
+/dev/null is the null device it takes any input you want and throws it away. It can be used to suppress any output. Example &>/dev/null
+
+
+# FORMAT TEXT AS TABLE
+Instead of using echo, Use printf command.
+You can use %s for string and %d for integer, something like this,
+printf " %s %d", $string1 $int1
+You can space according to your screen, by using %20s for using 20 characters for the string, %12d for the integer.
+Format control options are also available :
+\n : newline
+\t : tab(horizontal)
+\v : tab(vertical)
+# Example
+printf "Hello \t these \t are tabs \n"
+printf "And \t the n \t are makes a new line \n"
+
+
+# Create a variable to bold terminal text. This work just like opening an closing <bold></bold> tags in html
+BOLDFONT=$(tput bold)
+NORMALFONT=$(tput sgr0)
+
