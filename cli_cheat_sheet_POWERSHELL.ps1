95cb75ce02b7798b2ed285a0f8d0537cbca10e9e
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-08-28 16:07:12 -0500

    Update cli_cheat_sheet_POWERSHELL.ps1

--- cli_cheat_sheet_POWERSHELL.ps1
+++ cli_cheat_sheet_POWERSHELL.ps1
@@ -21,9 +21,21 @@ my comment
 # Prefix to call an environmental variable
 $Env:
 #ex: $Env:SystemRoot
+#ex: $env:USERPROFILE
 
 # How to find what version of PowerShell you are running:
 $psversiontable
 
 # Tail files in PS
 Get-Content ./log.log -Wait -Tail 10
+
+# How to create a logfile/send text to file
+$logFile = "globalCompressor.log"
+Add-Content $logFile -value "Log file has been established!"
+
+# How to get the name of the currently running script (this file). This will include the file extension.
+$MyInvocation.MyCommand.Name
+
+# Show date in 1460 in the past and another 1460 days in the future. This is usefull for creating cut-off dates:
+(Get-Date).AddDays(-1460)
+(Get-Date).AddDays(1460)
