d688ec1cad375cfa63be5982b6662ac9b33b4456
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2019-01-14 14:14:45 -0600

    Create cli_cheat_sheet_SQL.sql

---
+++ cli_cheat_sheet_SQL.sql
@@ -0,0 +1,33 @@
+/*
+ SSSSSSSSS	 QQQQQQQQ 	L
+S		Q	 Q	L
+S		Q	 Q	L
+ SSSSSSSS	Q	 Q	L
+	 S	Q     Q  Q	L
+	 S	Q	QQ	L
+SSSSSSSSS	 QQQQQQQQ Q	LLLLLLLLL
+*/
+
+
+-------------------
+-- MICROSOFT SQL --
+-------------------
+
+-- This is a single-line comment
+/* This 
+is
+a 
+multiline
+comment
+*/
+
+
+-- Trace the path/process of your query 
+SET SHOWPLAN_TEXT ON
+SET SHOWPLAN_XML ON
+SET SHOWPLAN_TEXT OFF
+SET SHOWPLAN_XML OFF
+
+
+
+
