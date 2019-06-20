635248643d679b181513d819f3ed379f60a2e9b0
MorningCoffeeZombie <ryan.jenson@wi.rr.com>
2019-06-20 15:51:41 -0500

    More scripts

--- clie_cheat_sheet_C++.cpp
+++ clie_cheat_sheet_C++.cpp
@@ -186,6 +186,14 @@ int main( int argc, char** argv[] ) {	// int main will need "int argc, char** ar
 }
 
 
-
+// Environmental storage locations:
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char** argv) {
+    printf("USERPROFILE = %s\n", getenv("USERPROFILE"));
+    printf("HOMEDRIVE   = %s\n", getenv("HOMEDRIVE"));
+    printf("HOMEPATH    = %s\n", getenv("HOMEPATH"));
+    return 0;
+}
 
 
