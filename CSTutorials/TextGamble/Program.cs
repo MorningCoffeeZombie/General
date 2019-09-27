2876d3e8bf1727dcb2056aec227e7fd56d8d81bb
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2019-09-27 02:53:30 -0500

    Creating a CLI odds generator

---
+++ CSTutorials/TextGamble/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;   // Used for the `List<double> grades` type
+
+namespace TextGamble {
+	class Program {
+		static void Main(string[] args){
+
+			Random rnd = new Random();
+			if(args[0].Equals("roll")) {
+				int intRoll = rnd.Next(1, 101); // Random integer 1-100
+				Console.WriteLine($"Random Roll: {intRoll}");
+			}
+
+			if(args[0].Equals("dice")) {
+				int intDice = rnd.Next(1, 7);   // Random integer 1-6
+				Console.WriteLine($"Random Dice: {intDice}");
+			}
+
+			if(args[0].Equals("coin") || args[0].Equals("coins")) {
+				int intCoin = rnd.Next(0, 2);   // Random integer 0-1
+				if(intCoin == 1){
+					Console.WriteLine($"Random Coin: {intCoin} (aka Tails)");
+				} else if(intCoin == 0){
+					Console.WriteLine($"Random Coin: {intCoin} (aka Heads)");
+				} else {
+					Console.WriteLine($"Random Coin is broken");
+				}
+			}
+
+			var lisCards = new List<string>{ 
+				"Ace of Hearts",
+				"King of Hearts",
+				"Queen of Hearts",
+				"Jack of Hearts",
+				"Ten of Hearts",
+				"Nine of Hearts",
+				"Eight of Hearts",
+				"Seven of Hearts",
+				"Six of Hearts",
+				"Five of Hearts",
+				"Four of Hearts",
+				"Three of Hearts",
+				"Two of Hearts",
+				"Ace of Clubs",
+				"King of Clubs",
+				"Queen of Clubs",
+				"Jack of Clubs",
+				"Ten of Clubs",
+				"Nine of Clubs",
+				"Eight of Clubs",
+				"Seven of Clubs",
+				"Six of Clubs",
+				"Five of Clubs",
+				"Four of Clubs",
+				"Three of Clubs",
+				"Two of Clubs",
+				"Ace of Spades",
+				"King of Spades",
+				"Queen of Spades",
+				"Jack of Spades",
+				"Ten of Spades",
+				"Nine of Spades",
+				"Eight of Spades",
+				"Seven of Spades",
+				"Six of Spades",
+				"Five of Spades",
+				"Four of Spades",
+				"Three of Spades",
+				"Two of Spades",
+				"Ace of Diamonds",
+				"King of Diamonds",
+				"Queen of Diamonds",
+				"Jack of Diamonds",
+				"Ten of Diamonds",
+				"Nine of Diamonds",
+				"Eight of Diamonds",
+				"Seven of Diamonds",
+				"Six of Diamonds",
+				"Five of Diamonds",
+				"Four of Diamonds",
+				"Three of Diamonds",
+				"Two of Diamonds"
+			};
+			if(args[0].Equals("card") || args[0].Equals("cards")) {
+				int intCard = rnd.Next(lisCards.Count);
+				Console.WriteLine($"Random Card: {lisCards[intCard]}");
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
