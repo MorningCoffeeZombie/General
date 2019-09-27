e54622a68aefa08acd8354bd9a2457a8c927dbe8
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2019-09-26 22:48:16 -0500

    4.9

--- CSTutorials/gradebook/src/GradeBook/Book.cs
+++ CSTutorials/gradebook/src/GradeBook/Book.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 namespace GradeBook {
 
     class Book {
-        public Book(){  // This is a constructor. Used for initializing (things) so program doesn't crash from null error (using a var that was never declared).
+        public Book(string name){  // This is a constructor. Used for initializing (things) so program doesn't crash from null error (using a var that was never declared).
             lisGrades = new List<double>();
+            this.name = name;   // The 'this.' is a way to reference the object currently being worked on rather than an outside variable of the same name
         }
 
         public void AddGrade(double grade){
             lisGrades.Add(grade);
         }
 
-        List<double> lisGrades; // This isn't inside of the 'void' but is inside of a class so it's called a 'field'. Voids hold local variables, these fields can be used throughtout the script.
+        private List<double> lisGrades; // This isn't inside of the 'void' but is inside of a class so it's called a 'field'. Voids hold local variables, these fields can be used throughtout the script.
+        private string name;
 
     }
 }
--- CSTutorials/gradebook/src/GradeBook/Program.cs
+++ CSTutorials/gradebook/src/GradeBook/Program.cs
@@ -6,8 +6,9 @@ namespace GradeBook {
         static void Main(string[] args){ // This is a method called "Main"
                                         // It also accepts an argument "arg" which is an array of strings (array due to brackets)
 
-            var varBook = new Book();    // This is referencing the class named "Book" in file "Book.cs". To not have errors, the class must exist (even if it's empty)
-            varBook.AddGrade(1.02);
+            var varBook = new Book("My Grade Book");    // This is referencing the class named "Book" in file "Book.cs". To not have errors, the class must exist (even if it's empty)
+            varBook.AddGrade(10.02);
+            //varBook.lisGrades.Add(101);   // This will only work if the field named "lisGrades" in Book.cs is set to 'public' access, not private
 
             double varX = 12.3;
             double varY = 32.1;
@@ -18,18 +19,24 @@ namespace GradeBook {
             List<double> lisGrades = new List<double>();    // Call in a namespace that will allow for an unknown/infinite array of doubles named 'grades'
                                                         // You may also use brackets to kickoff the array with content: {1, 2, 3, 4, };
                 lisGrades.Add(46.1);  // Insert value into List named 'lisGrades'
-                lisGrades.Add(1.0);
-                lisGrades.Add(1337.0);
+                lisGrades.Add(87.0);
+                lisGrades.Add(100.0);
                 lisGrades.Add(15.3);
                 lisGrades.Add(16.0);
 
             double varAnswer = 0;
+            var varHighGrade = double.MinValue;
+            var varLowGrade = double.MaxValue;
             foreach(double number in lisGrades){    // Sum all of the values in the arrGrades array
+                varHighGrade = Math.Max(number, varHighGrade);  // Use the Math. command to find the maximum number between the ones listed in the ()
+                varLowGrade = Math.Min(number, varLowGrade);
                 varAnswer += number;
                 Console.WriteLine($"You're at: {varAnswer} foreach. Current number in array is {number}");
             }
-            varAnswer /= lisGrades.Count;   // The /= is jus like += but for division. Also the ".Count" allows you to grab the total number of entities in an array/list
+            varAnswer /= lisGrades.Count;   // The /= is just like += but for division. Also the ".Count" allows you to grab the total number of entities in an array/list
             Console.WriteLine($"The average is: {varAnswer}");
+            Console.WriteLine($"The highest is: {varHighGrade}");
+            Console.WriteLine($"The lowest is: {varLowGrade}");
             
             if(args.Length > 0){ // Check if the argument has any characters in it (to make sure not empty)
                 Console.WriteLine("Hello there " + args[0] + ", these are concat. text strings!");
