9f908d2817fbe9fa809476bfd80da5218fc42336
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2019-09-28 03:40:04 -0500

    Unit tests

---
+++ CSTutorials/gradebook/test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // ARRANGE
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            // ACT
+           var varResult = book.GetStatistics();
+
+           
+            // ASSERT
+            Assert.Equal(85.6, varResult.Average, 1);   // The last argument is the 'precision'... how many decimals to expect/analyze
+            Assert.Equal(90.5,varResult.High, 1);
+            Assert.Equal(77.3,varResult.Low, 1);
+        }
+    }
+}
